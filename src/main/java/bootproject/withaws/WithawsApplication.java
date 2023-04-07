7ae9d847070e1224f1a329f0731984f20a3a5e05
jiwonjjang <ukkangsolo@gmail.com>
2023-04-07 12:05:44 +0900

    Initial commit

---
+++ src/main/java/bootproject/withaws/WithawsApplication.java
@@ -0,0 +1,13 @@
+package bootproject.withaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootproject/withaws/WithawsApplicationTests.java
@@ -0,0 +1,13 @@
+package bootproject.withaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
