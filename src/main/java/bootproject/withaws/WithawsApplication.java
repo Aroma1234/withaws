d0190a88535a3a1aa279fafd3e776119979ca7ba
jiwonjjang <ukkangsolo@gmail.com>
2023-04-12 12:30:15 +0900

    yml

--- src/main/java/bootproject/withaws/WithawsApplication.java
+++ src/main/java/bootproject/withaws/WithawsApplication.java
@@ -2,7 +2,9 @@ package bootproject.withaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class WithawsApplication {
 
